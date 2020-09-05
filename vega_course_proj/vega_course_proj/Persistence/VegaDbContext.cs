dd1f8f968ca3603f3a1c6238c0fc1e04abd91343
Andriy-KAP <andriykuklinov@gmail.com>
2020-09-05 22:06:52 +0300

    Adding model constraints.

--- vega_course_proj/vega_course_proj/Persistence/VegaDbContext.cs
+++ vega_course_proj/vega_course_proj/Persistence/VegaDbContext.cs
@@ -1,9 +1,12 @@
 using Microsoft.EntityFrameworkCore;
+using vega_course_proj.Models;
 
 namespace vega_course_proj.Persistence
 {
     public class VegaDbContext: DbContext
     {
+        public DbSet<Make> Makes { get; set; }
+        public DbSet<Model> Models { get; set; }
         public VegaDbContext(DbContextOptions<VegaDbContext> options)
             :base(options)
         {
