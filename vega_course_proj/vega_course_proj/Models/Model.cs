ed086eac76b2926558e3f4614d797aec89a4106a
Andriy-KAP <andriykuklinov@gmail.com>
2020-09-05 13:59:20 +0300

    Adding domain model classes.

---
+++ vega_course_proj/vega_course_proj/Models/Model.cs
@@ -0,0 +1,11 @@
+namespace vega_course_proj.Models
+{
+    public class Model
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public Make Make { get; set; }
+        public int MakeId { get; set; }
+    }
+}
