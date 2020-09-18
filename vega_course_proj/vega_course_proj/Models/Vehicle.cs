265ce27def5eaf0c29f9b11b2b6f9e8e8e26861f
Andriy-KAP <andriykuklinov@gmail.com>
2020-09-18 12:44:33 +0300

    Implementing vehicle model.

---
+++ vega_course_proj/vega_course_proj/Models/Vehicle.cs
@@ -0,0 +1,22 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace vega_course_proj.Models
+{
+    public class Vehicle
+    {
+        public int Id { get; set; }
+        public int ModelId { get; set; }
+        public Model Model { get; set; }
+        public bool IsRegistered { get; set; }
+        [Required]
+        [StringLength(255)]
+        public string ContactName { get; set; }
+        [Required]
+        [StringLength(255)]
+        public string ContactEmail { get; set; }
+        public string ContactPhone { get; set; }
+        public DateTime LastUpdate { get; set; }
+
+    }
+}
